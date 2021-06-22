7ea81f743626f65d8310f3ff26a5290416445c38
darkmageman4 <darkmageman4@yahoo.com>
2021-06-22 10:43:03 -0400

    syntax

---
+++ src/Syntax.java
@@ -0,0 +1,18 @@
+public class Syntax {
+
+    public static void main(String[] args) {
+
+
+//        int x = 4 + 5;
+//        x = x + 5;
+//
+//        int x = 3 * 4;
+//        int y = 4;
+//        y = y * x;
+//
+//        int x = 10 -2;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+    }
+}
