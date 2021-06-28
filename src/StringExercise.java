557d190345ae547f8c5ed1b5f75cc3eac26fbf78
darkmageman4 <darkmageman4@yahoo.com>
2021-06-28 12:16:37 -0400

    strings

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class StringExercise {
     public static void main(String[] args) {
-        String test = "We don't need no education\n" +
-                "We don't need no thought control";
-        System.out.println(test);
+//        String test = "We don't need no education\n" +
+//                "We don't need no thought control";
+//        System.out.println(test);
+        //        TODO: create a String variable that holds this sentence: "Red
+//         Orange Blue Yellow Green Purple". Prompt the user to type in
+//         their favorite color, if their fave color is the same as the
+//         first word in the String, then print out "my fave color:
+//         userColor is the first one". If their fave color is last, then print out "my fave color: userColor is last"
+        Scanner sc = new Scanner(System.in);
+        String pride = "Red Orange Blue Yellow Green Purple";
+        System.out.println("Whats your favorite color?");
+        String userInput = sc.next();
+        if (userInput.contentEquals("Red")) {
+            System.out.printf("My favorite color %s is the first one", userInput);
+        }else if (userInput.contentEquals("Purple")){
+            System.out.printf("My favorite color %s is the last one",userInput);
+        }else {
+            System.out.println("Get out of here!");
+        }
     }
 }
---
+++ src/morningExercises.java
@@ -0,0 +1,34 @@
+//        warmup:
+//        TODO: create a new "Strings-Lecture" java class file (or wherever
+//         you like to keep notes/lecture exercises. Copy given
+//         code, that is your starting point. Using Scanner prompt a
+//         user (in this case it's you, you're the user) to type in
+//         their first name. Save the input into a String variable. If
+//         your name matches randomName, print "woah! you guessed:
+//         enter-randomName-here! How did you know enter-yourName-here
+//         is my name?!". if randomName does not match your name exactly,
+//         print "Way off my friend! My name
+//         isn't randomName-here. It's yourName-here.".
+        import java.util.Scanner;
+        public class morningExercises {
+            public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                String[] studentsArr =
+                        {"Amado", "Christian", "Christopher", "Corey", "Demetrio",
+                                "Diamond", "Erik", "Grady", "Jesse", "Joshua",
+                                "Juan", "Mitchell", "Moses", "Nathan", "Prachi",
+                                "Raul", "Ricardo", "Richard", "Robert", "Sam",
+                                "Sean", "Stephen", "Tiffanny", "Wagner", "Laura",
+                                "Casey", "Dorian", "Tristan", "Jordy"};
+                int randomNum = (int) (Math.random() * 28);
+                String randomName = studentsArr[randomNum];
+                /* your code goes here */
+                System.out.println("Come out with your hands up! ");
+                String firstName = sc.next();
+                if (firstName.equals(randomName)){
+                    System.out.printf("woah you guessed it guessed: %s. How did you know %n %s is my name?!.", firstName, firstName);
+                }else {
+                    System.out.printf("Get off my front porch! My name isn't %s it's %s .", randomName ,firstName);
+                }
+            }
+        }
