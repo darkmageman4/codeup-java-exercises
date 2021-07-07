e05a0bbcc6386edd1b44e7f7f23268927e17c4aa
darkmageman4 <darkmageman4@yahoo.com>
2021-07-07 14:51:41 -0400

    initial

--- src/ServerNameGenerator.java
+++ src/ServerNameGenerator.java
@@ -1,2 +1,21 @@
 public class ServerNameGenerator {
+    public static void main(String[] args) {
+
+        String[] nouns = {"Bread", "Milk", "Juice", "Eggs", "Cheese", "Rice"
+                , "Grenades", "Ar-15", "Scar", "Sig"};
+
+        String[] adjective = {"adorable", "adventurous", "aggressive", "agreeable"
+                , "alert", "alive"
+                , "amused", "angry-15", "annoying", "annoyed"};
+
+        for (String Nouns : nouns) {
+            System.out.println(Nouns);
+        }
+        for (String Adjectives : nouns) {
+            System.out.println(Adjectives);
+
+        }
+
+//        System.out.println("Here is your server name: \n" + Nouns + Adjectives);
+    }
 }
