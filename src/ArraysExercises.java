24e49452bab6ce7393c2538c99a30f12a084772e
darkmageman4 <darkmageman4@yahoo.com>
2021-07-07 12:43:02 -0400

    Array exercises 1

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,19 +1,32 @@
+import oop.Person;
+
+import java.lang.reflect.Array;
 import java.util.Arrays;
+
 
 public class ArraysExercises {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5};
+//        int[] numbers = {1, 2, 3, 4, 5};
 //        System.out.println(Arrays.toString(numbers));
+        format();
 
-        String[] Person = new String [3];
-        Person[0] = "DamN";
-        Person[0] = "These";
-        Person[0] = "Eggs";
+addPerson();
+
+    }
+
+    public static void format() {
+
+        String[] Person = new String[3];
+        Person[0] = "Damn";
+        Person[1] = "These";
+        Person[2] = "Eggs";
 
         System.out.println(Person);
-//        for (String Person : Person){
-//            System.out.println(Person);
-
+        for (String n : Person) {
+            System.out.println(n);
 
         }
     }
+}
+
+
