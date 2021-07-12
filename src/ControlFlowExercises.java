bc7248bff26c3336edf25ded6cf6cccde67ea7e8
darkmageman4 <darkmageman4@yahoo.com>
2021-07-12 15:10:41 -0400

    Control

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 //public class ControlFlowExercises {
 //    public static void main(String[] args) {
-        //While loop
+//While loop
 //        int i = 5;
 //        while (i < 16){
 //            System.out.println("i is " + i);
 //            i++;
 //    }
-        // Do while loop
+// Do while loop
 //        int i = 2;
 //        do {
 //            System.out.println(i);
 //            i *= i;
 //        } while (i <= 65536);
-        //for loop
+//for loop
 //        for (long i = 2; i < 10000; i *= i){
 //            System.out.println(i);
 //            if (i == 5){
@@ -22,34 +22,68 @@ import java.util.Scanner;
 //            }
 //        }
 
-     import java.util.stream.IntStream;
+import java.util.stream.IntStream;
 
-        public class ControlFlowExercises {
-            public static void main(String[] args) {
-                fizzBuzzBeforeJava8(100);
-                fizzBuzzInJava8(100);
-            }
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        fizzBuzzBeforeJava8(100);
+//        fizzBuzzInJava8(100);
+//        tableOFPower();
+        classGrade();
+    }
 
-            private static void fizzBuzzBeforeJava8(int num) {
-                for (int i = 1; i <= num; i++) {
-                    if (((i % 5) == 0) && ((i % 7) == 0)) // Is it a multiple of 5 & 7?
-                        System.out.println("fizzbuzz");
-                    else if ((i % 5) == 0) // Is it a multiple of 5?
-                        System.out.println("fizz");
-                    else if ((i % 7) == 0) // Is it a multiple of 7?
-                        System.out.println("buzz");
-                    else
-                        System.out.println(i); // Not a multiple of 5 or 7
-                }
-            }
+    private static void fizzBuzzBeforeJava8(int num) {
+        for (int i = 1; i <= num; i++) {
+            if (((i % 5) == 0) && ((i % 7) == 0)) // Is it a multiple of 5 & 7?
+                System.out.println("fizzbuzz");
+            else if ((i % 5) == 0) // Is it a multiple of 5?
+                System.out.println("fizz");
+            else if ((i % 7) == 0) // Is it a multiple of 7?
+                System.out.println("buzz");
+            else
+                System.out.println(i); // Not a multiple of 5 or 7
+        }
+    }
 
-            private static void fizzBuzzInJava8(int num) {
-                IntStream.rangeClosed(1, 100)
-                        .mapToObj(i -> i % 5 == 0 ? (i % 7 == 0 ? "FizzBuzz" : "Fizz") : (i % 7 == 0 ? "Buzz" : i))
-                        .forEach(System.out::println);
-            }
+    private static void fizzBuzzInJava8(int num) {
+        IntStream.rangeClosed(1, 100)
+                .mapToObj(i -> i % 5 == 0 ? (i % 7 == 0 ? "FizzBuzz" : "Fizz") : (i % 7 == 0 ? "Buzz" : i))
+                .forEach(System.out::println);
+    }
 
+    public static void tableOFPower() {
+        Scanner rocketPunch = new Scanner(System.in);
+        System.out.println("What number would you like to go up to?");
+       int value = rocketPunch.nextInt();
+        System.out.println();
 
+        System.out.println("Here is your table!");
+        System.out.println("number | squared | cubed");
+        System.out.println("------ | ------- | -----");
+
+        for (int y = 1; y <= value; y++) {
+            System.out.println(y + "       | " + y*y + "         | " + y*y*y);
+        }
+    }
+
+    public static String classGrade(){
+        Scanner sleepyMonki = new Scanner(System.in);
+        System.out.println("What's your class percentage?");
+        int iykyk = sleepyMonki.nextInt();
+        if (iykyk >= 88){
+            System.out.println("You got an A!");
+            return "You got an A!";
+        } else if (iykyk >= 80){
+            return "You got a B!";
+        } else if (iykyk >= 67){
+            return "You got a C!";
+        } else if (iykyk >= 60){
+            return "You got a D!";
+        } else
+            return "You got a F!";
+    }
+
+}
 
 /////////////// //MINI EXERCISE//////////////////////////////////
 // TODO: create three unique usernames (user1, user2, user3).
@@ -74,4 +108,3 @@ import java.util.Scanner;
 /////////////////////////////////////////////////////////////////////
 
 
-    }
