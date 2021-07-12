43b0be170b2a91ec2f4a2ffa55e70fdf52d088a9
darkmageman4 <darkmageman4@yahoo.com>
2021-07-12 15:50:05 -0400

    OOP

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,6 +3,66 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-    private Scanner greenGoblin = new Scanner(System.in);
+    //fields
+    private Scanner scanner;
+    private int number;
+    private double dbl;
 
+    public Input(Scanner scanner, int number, double dbl) {
+        this.scanner = new Scanner(System.in);
+        this.number = number;
+        this.dbl = dbl;
+    }
+
+    public int getInt() {
+        System.out.println("Enter a integer");
+        return this.scanner.nextInt();
+    }
+
+
+    public int getInt(int min, int max) {
+        int Num;
+        do {
+            System.out.printf("enter a integer between %d and %d%n", min, max);
+            Num = scanner.nextInt();
+        } while (Num < min || Num > max);
+        return Num;
+    }
+
+
+    public String getString() {
+        return "Codeup";
+    }
+
+
+    public boolean isYesNo() {
+        String yankeeDoodle = scanner.next();
+        return !yankeeDoodle.equalsIgnoreCase("n");
+    }
+
+//    public boolean isYesNo() {
+//        String yankeeDoodle = scanner.next();
+//        if (yankeeDoodle.equalsIgnoreCase("n")){
+//            return false;
+//        }
+//        return true;
+//    }
+
+public double getDbl(double min, double max) {
+    this.dbl = getDbl();
+
+    if (dbl < min) {
+        System.out.println("Pick a number between " + min + " and " + max);
+        getDbl(min, max);
+    } else if (dbl > max) {
+        System.out.println("Pick a number between " + min + " and " + max);
+        getDbl(min, max);
+    }
+    return this.dbl;
+}
+
+    public double getDbl() {
+        double dbl = scanner.nextDouble();
+        return dbl;
+    }
 }
